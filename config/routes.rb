2e969dd8e58bab04b39d3478a797d92a073cdced
Isabelle Ferrier <ferrierisabelle@gmail.com>
2020-01-10 12:54:33 +0100

    Add calendar routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,4 @@
 Rails.application.routes.draw do
-  get 'calendar/new'
-  get 'calendar/create'
-  get 'calendar/show'
-  get 'calendar/delete'
   root to: 'pages#home'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :calendar, only: [:new, :create, :show, :destroy]
 end
