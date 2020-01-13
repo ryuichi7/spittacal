6701b1228a989fa24ada22d8b5cc828b888c2c4e
Isabelle Ferrier <ferrierisabelle@gmail.com>
2020-01-13 17:46:37 +0100

    Define a module Gem in the calendar model

--- app/controllers/calendar_controller.rb
+++ app/controllers/calendar_controller.rb
@@ -1,13 +1,34 @@
 class CalendarController < ApplicationController
   def new
+    @calendar = Calendar.new
   end
 
   def create
+    @calendar = Calendar.new(calendar_params)
+    if @calendar.save
+      redirect_to calendar_path(@calendar)
+    else
+      render :new
+    end
   end
 
   def show
+    set_calendar
   end
 
   def destroy
+    set_calendar
+    @calendar.destroy
+    redirect_to root_path
+  end
+
+  private
+
+  def calendar_params
+    params.require(:calendar).permit(:name)
+  end
+
+  def set_calendar
+    @calendar = Calendar.find_by(params[:id])
   end
 end
--- app/models/calendar.rb
+++ app/models/calendar.rb
@@ -1,4 +1,14 @@
+require 'icalendar'
+
+module Gem
+  def new
+    Icalendar::Calendar.new
+  end
+end
+
 class Calendar < ApplicationRecord
   has_many :bookings
   belongs_to :resource
+
+  include Gem
 end
